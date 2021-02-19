d5d031df1d5e053ad9740114f12dbedc4bdc28d9
Tim Weigel <timweigel@gmail.com>
2021-02-18 19:11:54 -0600

    Initial definition
    Flesh out structure in working notes (to later add to README). Very basic object construction in mixedMessage.js.

--- mixedMessages.js
+++ mixedMessages.js
@@ -7,6 +7,40 @@
 					different pieces of data.
 =============================================================================*/
 
+// Gonna make an object for this
 const messageMaker = {
+	_sqlErrors: {
+		messageTypes: [],
+		messageSegments: [],
+		messageVariables: [],
+	},
+
+	/*
+		Some getters and setters to start with
+	*/
+	get messageTypes() {
+		return this._sqlErrors.messageTypes;
+	},
+
+	set messageTypes(messageTypeIn) {
+		this._sqlErrors.messageTypes.push(messageTypeIn);
+	},
+
+	get messageSegments() {
+		return this._sqlErrors.messageType;
+	},
+
+	set messageSegments(messageSegmentIn) {
+		this._sqlErrors.messageSegments.push(messageSegmentIn);
+	},
+
+	get messageVariables() {
+		return this._sqlErrors.messageVariables;
+	},
+
+	set messageVariables(messageVariableIn) {
+		this._sqlErrors.messageVariables.push(messageVariableIn);
+	},
+
 	
 }
